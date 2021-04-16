caf961a2bd069f9da3dbbd25b050e329db7f498e
Steve Brown <stephen.brown@konghq.com>
2021-04-16 12:01:54 +0100

    script update

--- scripts/get_introspection_token.sh
+++ scripts/get_introspection_token.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-TOKEN=$(curl -s -X POST 'https://dev-48174301.okta.com/oauth2/ausl7og2ga5Q3ksCo5d6/v1/token' \
+TOKEN=$(curl -s -X POST 'https://******.okta.com/oauth2/ausl7og2ga5Q3ksCo5d6/v1/token' \
        --header 'content-type: application/x-www-form-urlencoded'  \
-      --data-urlencode 'client_id=0oal7k9csRqEwzuTM5d6' \
-      --data-urlencode 'client_secret=hg9VlgPvH5y_ThWE7cHGSdNwhJ5QflXxuR6HHbTa'  \
+      --data-urlencode 'client_id=***********' \
+      --data-urlencode 'client_secret=*********************'  \
       --data 'scope=groups' \
       --data-urlencode 'grant_type=client_credentials' | jq -r '.access_token')
 
 echo $TOKEN
 
-curl -v -H "Authorization: Bearer $TOKEN" https://api.kongcx.ninja/test
+curl -v -H "Authorization: Bearer $TOKEN" https://<kong-api>/test
